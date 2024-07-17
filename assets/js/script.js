72643d0ffdf42a3d7f428630f35abff717a63c64
Dani <daniela.vilcazuniga@gmail.com>
2024-07-17 17:29:52 -0400

    termine el footer y agregue algunos elementos js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,51 @@
+// Smooth scroll
+$(document).ready(function() {
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 900, function(){
+				window.location.hash = hash;
+			});
+		}
+	});
+});
+
 // Carrusel
 // $(".carousel").on('slide.bs.carousel', function () {
 //     alert('No sea ansioso');
 // });
 
+// $('.carousel').carousel({
+//     interval: 2000
+// });
+// const carousel = new bootstrap.Carousel('.Carousel')
 
-const myCarouselElement = document.querySelector('#myCarousel')
+// Si uno pasa el mouse por uno de los iconos, todos los parrafos desaparecerán y si
+// uno quita el mouse de algún icono, los parrafos volverán a aparecer.
 
-const carousel = new bootstrap.Carousel(myCarouselElement, {
-interval: 500,
-touch: false
-})
+$(document).ready(function(){
+    $(".iconos").click(function(){
+      $(".desaparece").fadeOut();
+    });
+
+    $(".iconos").mouseleave(function(){
+        $(".desaparece").fadeIn();
+      });
+  });
+
+//  Mensaje cuando uno le da click a boton enviar 
+
+$(document).ready(function(){
+    $("#boton").click(function(){
+    alert("Gracias por contactarte con nosotros");
+    });
+  });  
+
+// Tooltip
+  $(document).ready(function(){
+    $('[data-bs-toggle="tooltip"]').tooltip();   
+  });  
+
